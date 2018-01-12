ad0ca73ab125a21f773b95b32f32da6f94e56510
Tobias Kaulfuß <tobias.kaulfuss@denkwerk.com>
2018-01-12 23:10:26 +0100

    Eintrag von View an Controller übertragen

--- src/main/java/com/example/springtest4/controller/MainController.java
+++ src/main/java/com/example/springtest4/controller/MainController.java
@@ -1,10 +1,13 @@
 package com.example.springtest4.controller;
 
+import com.example.springtest4.dto.EntryDto;
 import com.example.springtest4.model.Item;
 import com.example.springtest4.model.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,6 +22,13 @@ public class MainController {
     public String index(Model model) {
         List<Item> allItems = itemRepository.findAll();
         model.addAttribute("allItems", allItems);
+        model.addAttribute("entryDto", new EntryDto());
+        return "index";
+    }
+
+    @PostMapping("/save" )
+    public String save(@ModelAttribute EntryDto entryDto) {
+
         return "index";
     }
 }
---
+++ src/main/java/com/example/springtest4/dto/EntryDto.java
@@ -0,0 +1,23 @@
+package com.example.springtest4.dto;
+
+public class EntryDto {
+
+    private String name;
+    private double menge;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMenge() {
+        return menge;
+    }
+
+    public void setMenge(double menge) {
+        this.menge = menge;
+    }
+}
